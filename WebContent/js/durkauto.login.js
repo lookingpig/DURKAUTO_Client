e70fa411f91ed3560ca76accfdbcc4a86916525f
lookingpig <lookingpig@gmail.com>
2014-07-03 18:51:26 +0800

    实现登陆功能

---
+++ WebContent/js/durkauto.login.js
@@ -0,0 +1,28 @@
+$(function($) {
+	$("#loginForm").submit(function(){
+		var obj = {};
+		obj.ServiceName = "Login";
+		obj.username = $("#username").val();
+		obj.password = $("#password").val();
+
+		$.ajax({
+            type : 'post',
+            url : 'Service',
+            data: 'Message=' + JSON.stringify(obj),
+            dataType : 'json',
+            async : false,
+            success : function(data) {
+                if ("000000000" == data.StateCode) {
+					location.href = "index.html";
+				} else {
+					alert("Fall code: " + data.StateCode);
+				}
+            },
+            error : function() {  
+                alert("登陆失败！");
+            }  
+        });
+
+        return false;
+	});	
+});
--- WebContent/js/durkauto.main.js
+++ WebContent/js/durkauto.main.js
@@ -1,15 +1,15 @@
+//当页面加载完成时调用
 $(document).ready(function() {
-	//初始化菜单
-    $("#menu").kendoMenu({
-		select: onMenuSelect
-	});
+	
 });
 	
 //当主菜单被选中时加载相应页面
 function onMenuSelect(e) {
 	var url = "";
+	var servicename = $(e).attr("name");
+	var tabname = $(e).text();
 
-	switch ($(e.item).children(".k-link").children().attr("name")) {
+	switch (servicename) {
 		case "appointment-inquiry" :
 		url = "appointment/inquiry.html";
 		break;
@@ -19,6 +19,22 @@ function onMenuSelect(e) {
 	}
 
 	if ("" != url) {
-		$("#main").load(url);
+		if (0 < $("#" + servicename).length) {
+			//在这里如果已经存在该页面则切换
+		} else {
+			$("#main_tab").append('<li><a href="#' + servicename + '" data-toggle="tab" id="' + servicename + '_tab">' + tabname + ' <span class=" icon-remove" onclick="closeMainTab(this);"></span></a></li>');
+			$("#main_panel").append('<div class="tab-pane fade" id="' + servicename + '"></div>');
+			$("#" + servicename).load(url);
+		}
 	}
+}
+
+//关闭主标签栏
+function closeMainTab(e) {
+	var id = $(e).parent().attr("id");
+	//删除标签
+	$("#" + id).remove();
+	//删除页面
+	id = id.substring(0, id.indexOf("_tab"));
+	$("#" + id).remove();
 }
